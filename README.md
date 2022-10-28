27c1d6ff7e9988f533992c0b25b5fbfdff23feb0
Maria <vilochka-17@yandex.ru>
2022-10-28 16:01:39 +0300

    second Task made

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+Задача для самостоятельного решения 1
+
+[Блок-Схема](Task_1/diagram.png)  [Код](Task_1/Program.cs)
+
 Задача для домашнего решения 1
 
-[Блок-Схема](Task_1/diagram.png)
-
-[Код](Task_1/Program.cs)
+[Блок-Схема](Task_2/diagram2.png)  [Код](Task_2/Program.cs)
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 22, b = 3, c = 9, max = a;
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+else
+{
+    max = a;
+}
+Console.WriteLine(max);
