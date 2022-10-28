ffd976a418eb559e6d7866f83c061c4558bf2c5b
Maria <vilochka-17@yandex.ru>
2022-10-28 19:08:41 +0300

    Third Task made

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int Number = 7;
+string answer;
+if(Number % 2 == 0)
+{
+   answer = ("yes");
+}
+else
+{
+    answer = ("no");
+}
+Console.WriteLine(answer);
+
