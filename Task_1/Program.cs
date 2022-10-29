6f8c5d16a6e560cb82604b177ae4b8665ab18f4e
Maria <vilochka-17@yandex.ru>
2022-10-29 16:17:11 +0300

    Changing in Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,13 +1,14 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
-int a = -9, b = -3, max = 0;
+int a = -9, b = -3, max = 0, min = 0;
 if (a>b)
 {
-    max = a;
+    max = a; min = b;
 }
 else
 {
-    max = b;
+    max = b; min = a;
 }
-Console.WriteLine(max);
+Console.WriteLine($"{max} большее ");
+Console.WriteLine($"{min} меньшее ");
 
