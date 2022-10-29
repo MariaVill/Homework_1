c9d6560a6c216902f3367113e7befaf5fbf3e88c
Maria <vilochka-17@yandex.ru>
2022-10-29 16:36:12 +0300

    Mistakes 2

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,8 +1,17 @@
 ﻿// Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до N.
 
+Console.WriteLine("Hello, User! Please enter your name: ");
+string name = Console.ReadLine();
+Console.Write("Hello, ");
+Console.WriteLine(name);
+Console.Write(name);
+Console.Write(", please enter the number: ");
+int number = Int32.Parse (Console.ReadLine());
+Console.Write("i will find all even numbers before: ");
+Console.WriteLine(number);
 int N = 1;
-int size = 8;
+int size = number;
 while (N <= size)
 {
     if (N % 2 == 0)
